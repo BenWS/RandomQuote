fcd4c7a174eba199a4d08997539bbc13465c33eb
Shippey <BENJAMIN_W_SHIPPEY@homedepot.com>
2017-10-22 18:11:34 -0400

    Created server.js file for hosting the application on Heroku

---
+++ server.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var express = require('express');
+var path = require('path');
+var multer = require('multer');
+var upload = multer();
+var app = express();
+
+app.use(express.static('resources'));
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'));
+})
+
+// app.listen(process.env.PORT);
+app.listen(process.env.PORT);
